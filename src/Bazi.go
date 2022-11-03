9f3228090f2b161efb7c33dc0d311560abb7b7b4
Mohammad Mahdi Soozandeh <mo.m10003@gmail.com>
2022-11-03 15:11:35 +0330

    minor

--- src/Bazi.go
+++ src/Bazi.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 func main() {
+
+	rand.Seed(time.Now().UnixNano())
+	x := rand.Intn(1000)
+	//println("x", x)
+	var n int
+
+MainLoop:
+	for i := 25; i > 0; i-- {
+		println("chance", i)
+		fmt.Scanln(&n)
+		switch {
+		case n < x:
+			println("go up!")
+		case n > x:
+			println("go down!")
+		default:
+			break MainLoop
+		}
+	}
 
 }
